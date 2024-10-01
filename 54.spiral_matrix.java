07edff028b11a8d43b2fa79deb2568dd78b6780d
Deepthi S <130971935+DeepthiShiv@users.noreply.github.com>
2024-10-01 20:34:16 +0530

    Create Spiral_matrix.java
    Problem number 54. Spiral Matrix 
    Given an m x n matrix, return all elements of the matrix in spiral order.
    Example 1:
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [1,2,3,6,9,8,7,4,5]
    
    Example 2:
    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
    
    Constraints:
    
    m == matrix.length
    n == matrix[i].length
    1 <= m, n <= 10
    -100 <= matrix[i][j] <= 100

---
+++ 54.spiral_matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                result.add(matrix[startRow][j]);
+            }
+            startRow++;
+
+            // right
+            for (int i = startRow; i <= endRow; i++) {
+                result.add(matrix[i][endCol]);
+            }
+            endCol--;
+
+            // bottom (check if still within the same row)
+            if (startRow <= endRow) {
+                for (int j = endCol; j >= startCol; j--) {
+                    result.add(matrix[endRow][j]);
+                }
+                endRow--;
+            }
+
+            // left (check if still within the same column)
+            if (startCol <= endCol) {
+                for (int i = endRow; i >= startRow; i--) {
+                    result.add(matrix[i][startCol]);
+                }
+                startCol++;
+            }
+        }
+
+        return result;
+    }
+}
