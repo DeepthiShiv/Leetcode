1e5e59edecbd32f20f7c46018cbff81fdd3ea1fd
Deepthi S <130971935+DeepthiShiv@users.noreply.github.com>
2024-11-08 21:40:26 +0530

    Create DifferenceOfSum.c
    Difference Between Element Sum and Digit Sum of an Array

---
+++ DifferenceOfSum.c
@@ -0,0 +1,11 @@
+int differenceOfSum(int* nums, int numsSize) {
+    int sum1=0,sum2=0;
+    for(int i=0;i<numsSize;i++){
+        sum1+=nums[i];
+        while(nums[i]>0){
+            sum2+=nums[i]%10;
+            nums[i]/=10;
+        }
+    }
+    return sum1-sum2;
+}
